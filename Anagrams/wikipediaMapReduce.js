7e710e5d3153629dc725f9750725d33d1e41470d
klamkiewicz <krzysztof.lamkiewicz@gmail.com>
2015-01-04 22:34:17 +0100

    wikipedia mapReduce

---
+++ Anagrams/wikipediaMapReduce.js
@@ -0,0 +1,30 @@
+var MongoClient = require('mongodb').MongoClient
+, format = require('util').format;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/Mini', {db: {native_parser: true}}, function(err, db) {
+if(err) throw err;
+
+	db.collection('mini', function(err, col){
+		
+		var map = function(){
+			var words = this.text.split(/\s+/);
+			words.forEach(function(key){
+				emit(key, key);
+			});
+		};
+
+		var reduce = function(key, value){
+			return value.toString();
+		};
+
+		var finalize = function(key, value){
+			return value.length;
+		}
+
+		col.mapReduce(map, reduce, {
+			out: "words",
+			finalize: finalize
+		});	
+
+	});
+})
