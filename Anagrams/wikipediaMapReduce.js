4abe34579acb5376231a940b540916b0206edefb
klamkiewicz <krzysztof.lamkiewicz@gmail.com>
2015-01-06 19:10:49 +0100

    Fix

--- Anagrams/wikipediaMapReduce.js
+++ Anagrams/wikipediaMapReduce.js
@@ -5,25 +5,28 @@ MongoClient.connect('mongodb://127.0.0.1:27017/CleanWiki', {db: {native_parser: 
 if(err) throw err;
 
 	db.collection('Articles', function(err, col){
-		
+
+// jsMode: true
+
 		var map = function(){
-			var words = this.text.split(/\s+/);
+			var words = this.text.split(" ");
 			words.forEach(function(key){
-				emit(key, key);
+					emit(key, key);
 			});
 		};
 
 		var reduce = function(key, value){
-			return value.toString();
+			return value.length;
 		};
 
 		var finalize = function(key, value){
-			return value.length;
+			return key===value?1:value;
 		}
 
 		col.mapReduce(map, reduce, {
 			out: "words",
-			finalize: finalize
+			finalize: finalize,
+
 		});	
 
 	});
